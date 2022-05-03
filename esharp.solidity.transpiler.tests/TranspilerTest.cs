506f7a7bd2a7a09068c9c7491a3e781fe488382a
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2022-05-03 18:51:08 +1000

    WIP

--- esharp.solidity.transpiler.tests/TranspilerTest.cs
+++ esharp.solidity.transpiler.tests/TranspilerTest.cs
@@ -7,6 +7,21 @@ namespace esharp.solidity.transpiler.tests
     public class TranspilerTest
     {
         [Fact]
+        public void Should_Add_License_And_Pragma()
+        {
+            // Arrange
+            string source = @"Examples/Greeter.es";
+
+            // Act
+            Transpiler transpiler = new Transpiler(source);
+
+            // Assert
+            Assert.Equal(3, transpiler.Lines.Count);
+            Assert.Equal(@"// SPDX-License-Identifier: Unlicense", transpiler.Lines[0]);
+            Assert.Equal(@"pragma solidity ^0.8.0;", transpiler.Lines[2]);
+        }
+
+        [Fact]
         public void Should_Transform_Imports()
         {
             // Arrange
@@ -17,8 +32,8 @@ namespace esharp.solidity.transpiler.tests
             transpiler.TransformImports();
 
             // Assert
-            Assert.Equal(1, transpiler.Lines.Count);
-            Assert.Equal(@"import ""hardhat/console.sol"";", transpiler.Lines[0]);
+            Assert.Equal(4, transpiler.Lines.Count);
+            Assert.Equal(@"import ""hardhat/console.sol"";", transpiler.Lines[3]);
         }
     }
 }
